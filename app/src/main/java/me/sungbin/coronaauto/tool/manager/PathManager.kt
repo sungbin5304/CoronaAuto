fc282cd7be0315bcbf4c2eb7624624e050b5bb7d
sungbin5304 <sungbin.dev@gmail.com>
2020-09-12 23:29:16 +0900

    :unicorn:

--- app/src/main/java/me/sungbin/coronaauto/tool/manager/PathManager.kt
+++ app/src/main/java/me/sungbin/coronaauto/tool/manager/PathManager.kt
@@ -3,7 +3,7 @@ package me.sungbin.coronaauto.tool.manager
 object PathManager {
 
     const val MAIN = "corona-auto"
-    const val SCHOOL = "$MAIN/School Data/"
-    const val STUDENT = "$MAIN/Student Data/"
+    const val SCHOOL = "$MAIN/School Data"
+    const val STUDENT = "$MAIN/Student Data"
 
 }
--- app/src/main/java/me/sungbin/coronaauto/ui/activity/JoinActivity.kt
+++ app/src/main/java/me/sungbin/coronaauto/ui/activity/JoinActivity.kt
@@ -1,8 +1,11 @@
 package me.sungbin.coronaauto.ui.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import com.sungbin.sungbintool.util.PermissionUtil
 import com.sungbin.sungbintool.util.StorageUtil
 import com.sungbin.sungbintool.util.ToastUtil
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,7 +16,6 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import me.sungbin.coronaauto.R
 import me.sungbin.coronaauto.tool.manager.PathManager
-import me.sungbin.coronaauto.ui.dialog.LoadingDialog
 import org.json.JSONObject
 import org.jsoup.Connection
 import javax.inject.Inject
@@ -23,10 +25,6 @@ class JoinActivity : AppCompatActivity() {
 
     @Inject
     lateinit var client: Connection
-
-    private val loadingDialog by lazy {
-        LoadingDialog(this)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,44 +52,86 @@ class JoinActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, areas)
         actv_area.setAdapter(adapter)
 
-        btn_start.setOnClickListener {
-            StorageUtil.createFolder(PathManager.MAIN, true)
+        btn_request_storage.setOnClickListener {
+            PermissionUtil.request(
+                this,
+                null,
+                arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            )
+        }
 
-            val school = tiet_school.text.toString()
-            val area = actv_area.text.toString()
-            val name = tiet_name.text.toString()
-            val birth = tiet_birth.text.toString()
+    }
 
-            if (school.isNotBlank() && area.isNotBlank() && name.isNotBlank() && birth.isNotBlank()) {
-                CoroutineScope(Dispatchers.Main).launch {
-                    val value = async {
-                        client.data("schulNm", school)
-                            .data("geoNm", area)
-                            .get().text()
-                    }
-                    val json = JSONObject(value.await())
-                    val schoolCode = json["schulNm"]
-                    val schoolAddress = json["url"]
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            btn_request_storage.apply {
+                alpha = 0.5f
+                text = context.getString(R.string.join_permission_granted)
+            }
+            btn_start.apply {
+                alpha = 1f
+                setOnClickListener {
+                    StorageUtil.createFolder(PathManager.MAIN, true)
 
-                    if (schoolCode == getString(R.string.join_school_search_none)) {
+                    val school = tiet_school.text.toString()
+                    val area = actv_area.text.toString()
+                    val name = tiet_name.text.toString()
+                    val birth = tiet_birth.text.toString()
+
+                    if (school.isNotBlank() && area.isNotBlank() && name.isNotBlank() && birth.isNotBlank()) {
+                        CoroutineScope(Dispatchers.Main).launch {
+                            val schoolData = async {
+                                client.data("schulNm", school).data("geoNm", area).get().text()
+                            }
+                            val schoolJson = JSONObject(schoolData.await())
+                            val schoolCode = schoolJson["schulNm"]
+
+                            if (schoolCode == getString(R.string.join_school_search_none)) {
+                                ToastUtil.show(
+                                    applicationContext,
+                                    getString(R.string.join_school_search_error),
+                                    ToastUtil.SHORT, ToastUtil.WARNING
+                                )
+                            } else {
+                                val studentData = async {
+                                    client.data("pName", name).data("frnoRidno", birth).get().text()
+                                }
+                                StorageUtil.save(
+                                    "${PathManager.SCHOOL}/$school.json",
+                                    schoolJson.toString(),
+                                    true
+                                )
+                                StorageUtil.save(
+                                    "${PathManager.STUDENT}/$name.json",
+                                    studentData.await(),
+                                    true
+                                )
+                                ToastUtil.show(
+                                    applicationContext,
+                                    getString(R.string.join_welcome),
+                                    ToastUtil.SHORT, ToastUtil.WARNING
+                                )
+                            }
+                        }
+                    } else {
                         ToastUtil.show(
                             applicationContext,
-                            getString(R.string.join_school_search_error),
-                            ToastUtil.SHORT, ToastUtil.WARNING
+                            getString(R.string.join_input_all),
+                            ToastUtil.SHORT,
+                            ToastUtil.WARNING
                         )
                     }
-                    else {
-                        StorageUtil.save()
-                    }
                 }
-            } else {
-                ToastUtil.show(
-                    applicationContext,
-                    getString(R.string.join_input_all),
-                    ToastUtil.SHORT,
-                    ToastUtil.WARNING
-                )
             }
         }
     }
+
 }
